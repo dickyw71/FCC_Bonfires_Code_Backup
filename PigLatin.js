c3b16c4fe9e4955c2c95e7a1d1f6ffdaf85a8d43
Richard Wheatley <dickyw71@users.noreply.github.com>
2015-12-29 22:03:14 +0000

    My final solution
    Changed if-else to switch and handle words that don’t have a vowel.

--- PigLatin.js
+++ PigLatin.js
@@ -2,17 +2,21 @@ function translate(str) {
   
   var vowelIndex = str.search(/[aeiou]/);
   
-  if(vowelIndex === 0) {
-    str = str + "way";
+  switch(vowelIndex) {
+    case -1:  // word does not have a vowel!
+     break;
+    case 0:   //  word starts with a vowel - add 'way'
+      str = str + "way";
+      break;
+    default:  // slice and dice word into pig latin
+      var firstConsonants = str.slice(0, vowelIndex);
+      var restOfWord = str.slice(vowelIndex);
+      str = restOfWord + firstConsonants + "ay";      
+      break;
   }
-  else {
-    var firstConsonants = str.slice(0, vowelIndex);
-    var restOfWord = str.slice(vowelIndex);
-    str = restOfWord + firstConsonants + "ay";
-  }
-  
 
   return str;
 }
 
 translate("consonant");
+
