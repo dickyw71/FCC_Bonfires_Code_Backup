bdb54ef114e72fe8da58f0b3f1bbda55892f2e60
Richard Wheatley <dickyw71@users.noreply.github.com>
2016-02-01 22:42:07 +0000

    Improved solution
    More concise solution with only 1 while loop that both calculates the
    Fibonacci numbers and sums the Odd ones.

--- SumAllOddFibonacciNumbers.js
+++ SumAllOddFibonacciNumbers.js
@@ -1,35 +1,18 @@
 function sumFibs(num) {
   
-  // calculate all Fibonacci Numbers upto and including num
-  //  Starting with 0,1,1 : Fn = Fn-1 + Fn-2
-  var fibArr = [0,1,1];
-  while(num > fibArr[fibArr.length-1]) {
-    fibArr.push(fibArr[fibArr.length-1] + fibArr[fibArr.length-2]);
-  }
-  
-  console.log(fibArr.length);
-  console.log(fibArr);
-  
-  // calculate sum of all odd Fibonacci Numbers in fibArr
-  var oddSum = 0;
-  var evenSum = 0;
-  var index = 0;
+  var prevFibNum = 1;
+  var currFibNum = 1;
+  var nextFibNum;
+  var oddSum = 1; // because sum of first 2 Fibonacci numbers in sequence is 0+1 = 1
 
-  while (fibArr[index] <= num ) {
-    
-    if(fibArr[index] % 2) {
-      // Odd Fib# Value
-      oddSum += fibArr[index];
+  while ( currFibNum <= num ) {
+    if (currFibNum % 2) {    // Check if Fibonacci number is Odd
+      oddSum += currFibNum;
     }
-    else {
-      evenSum += fibArr[index];
-    }
-    
-    index++;  // next Fib# in array
+    nextFibNum = prevFibNum + currFibNum; 
+    prevFibNum = currFibNum;        
+    currFibNum = nextFibNum;       
   }
-  
-  console.log("Odd Sum: " + oddSum);
-  console.log("Even Sum: " + evenSum);
   
   return oddSum;
 }
